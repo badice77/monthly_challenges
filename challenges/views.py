f9832f71ea7ebdd482f1e8ed0e9fe9955f56ee01
Yannick <y.morizot@numericable.com>
2021-12-31 16:29:17 +0100

    tags and the for tag, ajout du fichier index

--- challenges/views.py
+++ challenges/views.py
@@ -48,7 +48,10 @@ def monthly_challengeby_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "text_month": month
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not yet supported</h1>")
     
