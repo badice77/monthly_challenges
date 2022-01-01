158c2ff5519248a99cc807cf12213e48021dbe17
Yannick <y.morizot@numericable.com>
2022-01-01 16:00:27 +0100

    44. Including Partial Template Snippets

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index), #/challenges/
+    path("", views.index, name = "index"), #/challenges/
     path('<int:month>', views.monthly_challengeby_number),
     path('<str:month>', views.monthly_challenge, name="month-challenge"),
 ]
